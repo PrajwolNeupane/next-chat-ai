aa4b78e807b22e180f01ce07af17b9c2cd6051d7
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-31 23:22:02 +0545

    Chat Routes Defined

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Page() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS ChatGPT Starter</title>
+      </Head>
+      <h1>Welcome to the ChatGPT ID PAGE</h1>
+    </div>
+  );
+}
