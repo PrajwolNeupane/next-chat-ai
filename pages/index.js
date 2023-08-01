be524c5a64b32c9870873812e605d377dc108c42
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-01 21:46:06 +0545

    Added LogOut Action

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
 import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
+
+  const {isLoading,error,user} = useUser();
+
+  if(isLoading) return <div>Loading...</div>
+  if(error) return <div>{error.message}</div>
+
   return (
     <div>
       <Head>
@@ -9,9 +16,12 @@ export default function Home() {
       </Head>
       <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
       <div>
-        <Link href={"/api/auth/login"}>
-        Login
-        </Link>
+        {
+          !!user && <Link href={"/api/auth/logout"}>LogOut</Link>
+        }
+        {
+          !user &&    <Link href={"/api/auth/login"}>Login</Link>
+        }
       </div>
     </div>
   );
