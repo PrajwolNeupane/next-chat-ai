af38fd62e6b47423b1a7b0ff90408a8452025b1a
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-05 22:00:41 +0545

    Finished

--- pages/api/chat/sendmessage.js
+++ pages/api/chat/sendmessage.js
@@ -7,24 +7,31 @@ export const config = {
 export default async function handler(req) {
     try {
         const { message } = await req.json();
-        const stream = await OpenAIEdgeStream("https://api.openai.com/v1/chat/completions",{
-            headers:{
-                'content-type':'application/json',
-                Authorization:`Bearer ${process.env.OPENAI_API_KEY}`
-            },
-            method:"POST",
-            body:JSON.stringify({
-                model:"gpt-3.5-turbo",
-                messages:[{content:message,role:"user"}],
-                stream:true
-            })
-        });
+        if (message) {
 
-        return new Response(stream);
+            const stream = await OpenAIEdgeStream("https://api.openai.com/v1/chat/completions", {
+                headers: {
+                    'content-type': 'application/json',
+                    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    model: "gpt-3.5-turbo",
+                    messages: [{ content: message, role: "user" }],
+                    stream: true
+                })
+            });
+
+            return new Response(stream);
+        }
+        else {
+            return new Response.json({ error: "No Message" })
+        }
 
     } catch (e) {
         console.log("Error on Sending Message |");
         console.log(e);
+        return new Response.json({ error: e })
     }
 
 
--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -2,34 +2,73 @@ import Head from "next/head";
 import { ChatSideBar } from "components";
 import { useState } from "react";
 import { streamReader } from "openai-edge-stream";
+import { v4 as uuid } from 'uuid';
 
 export default function ChatPage() {
 
+  const [inComingMessage, setInComingMessage] = useState("");
   const [messageText, setMessageText] = useState("");
+  // const [newChatMessages, setNewChatMessages] = useState([]);
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`/api/chat/sendmessage`,
-      {
+    try {
+      const response = await fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         headers: {
-          'content-type': "application/json"
+          'content-type': "application/json",
+          //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
+          Authorization: `Bearer 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`
         },
         body: JSON.stringify({
-          message: messageText
+          model: "gpt-3.5-turbo",
+          message: [{ content: messageText, role: "user" }]
         })
-      });
-    const data = response.body
-    if (!data) {
-      console.log("No Data");
+      })
+      const data = await response.json();
       console.log(data);
-      return;
+    } catch (e) {
+      console.log(e);
     }
-    const reader = data.getReader();
-    await streamReader(reader,(message)=>{
-      console.log(message);
-    })
   }
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     // setNewChatMessages(prev => {
+  //     //   const newChatMessage = [...prev, {
+  //     //     _id: uuid,
+  //     //     role: 'user',
+  //     //     content: messageText
+  //     //   }]
+  //     // })
+  //     console.log(messageText)
+  //     const response = await fetch(`/api/chat/sendmessage`,
+  //       {
+  //         method: "POST",
+  //         headers: {
+  //           'content-type': "application/json"
+  //         },
+  //         body: JSON.stringify({
+  //           message: messageText
+  //         })
+  //       });
+  //     const data = response.body
+  //     if (!data) {
+  //       console.log("No Data");
+  //       console.log(data);
+  //       return;
+  //     }
+  //     const reader = data.getReader();
+  //     await streamReader(reader,  (message) => {
+  //       console.log(message);
+  //       setInComingMessage(s => `${s}${message.content}`);
+  //     })
+  //   }catch(e){
+  //     console.log(e);
+  //   }
+  // }
 
   return (
     <>
@@ -39,7 +78,9 @@ export default function ChatPage() {
       <div className="grid h-screen grid-cols-[260px_1fr]">
         <ChatSideBar />
         <div className="bg-gray-700 flex flex-col">
-          <div className="flex-1">Chat Window</div>
+          <div className="flex-1 text-white">
+            {inComingMessage}
+          </div>
           <footer className="bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
               <fieldset className="flex gap-2">
